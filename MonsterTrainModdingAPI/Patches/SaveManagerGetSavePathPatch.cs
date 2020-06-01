8f683d6d4e090579fd97474ac01414c32251d5a4
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2020-06-01 13:25:10 -0400

    Created PluginManager and added patches to register plugins.

---
+++ MonsterTrainModdingAPI/Patches/SaveManagerGetSavePathPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using MonsterTrainModdingAPI.Managers;
+
+namespace MonsterTrainModdingAPI.Patches
+{
+    [HarmonyPatch(typeof(SaveManager), "GetGameSavePath")]
+    class SaveManagerGetSavePathPatch
+    {
+        static void Postfix(ref string __result, SaveManager __instance)
+        {
+            string newFileName = "";
+            foreach(string s in PluginManager.GetAllPluginNames())
+            {
+                
+            }
+            __result += "..\\" + newFileName + ".json";
+        }
+    }
+}
