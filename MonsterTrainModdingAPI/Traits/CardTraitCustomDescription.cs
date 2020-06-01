372346a81db8fd94e589926d666e70a473482558
KittenAqua <mnpntgt@gmail.com>
2020-06-01 17:56:37 -0500

    Add custom card description support via a new trait

---
+++ MonsterTrainModdingAPI/Traits/CardTraitCustomDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using MonsterTrainModdingAPI.Managers;
+
+namespace MonsterTrainModdingAPI.Traits
+{
+    public class CardTraitCustomDescription : CardTraitState
+    {
+        public override string GetCardText()
+        {
+            var relicManager = (RelicManager)AccessTools.Field(typeof(CardState), "relicManager").GetValue(base.GetCard());
+            string baseDesc = base.GetCardTraitData().GetParamStr();
+            I2.Loc.LocalizationManager.ApplyLocalizationParams(ref baseDesc, null, new CardEffectLocalizationContext(base.GetCard(), relicManager, CustomCardManager.SaveManager, null));
+            return baseDesc;
+        }
+    }
+}
