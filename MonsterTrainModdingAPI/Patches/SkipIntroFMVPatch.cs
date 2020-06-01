446b501abb8b345f0503663e1f17db2a07fe864e
Bernhard Pasching <bernhard.pasching@gmail.com>
2020-06-01 15:59:49 +0200

    Added example patch for skipping the intro FMV

---
+++ MonsterTrainModdingAPI/Patches/SkipIntroFMVPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace MonsterTrainModdingAPI.Patches
+{
+    [HarmonyPatch(typeof(IntroFmvScreen), "Initialize")]
+    class IntroFmvScreen_Initialize_Patch
+    {
+        static bool Prefix(ref IntroFmvScreen __instance)
+        {
+            __instance.Invoke("GoToNextScreen", 0);
+            return false;
+        }
+    }
+}
