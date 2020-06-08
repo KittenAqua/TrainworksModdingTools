631b5612977cea5c168d44d5d6420cd67cc0318e
KittenAqua <35976163+KittenAqua@users.noreply.github.com>
2020-06-07 22:57:05 -0500

    Custom card and character art (#17)
    * Add support for custom card and character art
    
    * Change representation of custom character data
    
    * Clean up and simplify custom card and character asset loading and registration

---
+++ MonsterTrainModdingAPI/Patches/LoadCustomCardArtPatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using ShinyShoe;
+using Spine.Unity;
+using MonsterTrainModdingAPI.Managers;
+using TMPro;
+
+namespace MonsterTrainModdingAPI.Patches
+{
+    [HarmonyPatch(typeof(CardData), "GetCardArtPrefabVariant")]
+    class LoadCustomCardArtPatch
+    {
+        static void Prefix(ref CardData __instance)
+        {
+            if (!__instance.HasCardArtPrefabVariant() && CustomCardManager.CustomCardData.ContainsKey(__instance.GetID()))
+            {
+                CustomCardManager.CreateCardGameObject(__instance.GetID());
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterData), "GetCharacterPrefabVariant")]
+    class LoadCustomCharacterArtPatch
+    {
+        static void Prefix(ref CharacterData __instance, ref GameObject __result)
+        {
+            if (!__instance.HasCharacterPrefabVariant() && CustomCharacterManager.CustomCharacterData.ContainsKey(__instance.GetID()))
+            {
+                CustomCharacterManager.CreateCharacterGameObject(__instance.GetID());
+            }
+        }
+    }
+}
