9dfd0d58b85a2da6545210766d5ae0217db5e01b
KittenAqua <35976163+KittenAqua@users.noreply.github.com>
2020-07-06 21:11:51 -0400

    Custom reward data (#36)
    * Implement support for custom map nodes
    
    * Implement custom banner support
    
    * Add custom map nodes to map node pool via transpiler

---
+++ MonsterTrainModdingAPI/Builders/MapNodeBuilders/DraftRewardDataBuilder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Harmony;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using ShinyShoe;
+using MonsterTrainModdingAPI.Managers;
+using MonsterTrainModdingAPI.Builders;
+using MonsterTrainModdingAPI.Enums.MTStatusEffects;
+
+namespace MonsterTrainModdingAPI.Builders
+{
+    public class DraftRewardDataBuilder : IRewardDataBuilder
+    {
+        public string DraftRewardID { get; set; }
+
+        /// <summary>
+        /// Name of the reward data
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Description of the reward data
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Name title key, overridden by Name
+        /// </summary>
+        public string _RewardTitleKey { get; set; }
+        /// <summary>
+        /// Description title key, overridden by Description
+        /// </summary>
+        public string _RewardDescriptionKey { get; set; }
+
+        public ClassData ClassDataOverride { get; set; }
+        public RunState.ClassType ClassType { get; set; }
+        public bool ClassTypeOverride { get; set; }
+        /// <summary>
+        /// Number of cards the banner offers
+        /// </summary>
+        public uint DraftOptionsCount { get; set; }
+        /// <summary>
+        /// Card pool the banner pulls from
+        /// </summary>
+        public CardPool DraftPool { get; set; }
+        public bool GrantSingleCard { get; set; }
+        public CollectableRarity RarityFloorOverride { get; set; }
+        public bool UseRunRarityFloors { get; set; }
+
+        public bool CanBeSkippedOverride { get; set; }
+        public bool ForceContentUnlocked { get; set; }
+        public SaveManager SaveManager { get; set; }
+
+        public int[] Costs { get; set; }
+        public bool ShowRewardAnimationInEvent { get; set; }
+        public string _CollectSFXCueName { get; set; }
+        public bool _IsServiceMerchantReward { get; set; }
+        public Sprite _RewardSprite { get; set; }
+        public bool _ShowCancelOverride { get; set; }
+        public bool _ShowRewardFlowInEvent { get; set; }
+
+        public DraftRewardDataBuilder()
+        {
+            this.Costs = new int[0];
+        }
+
+        /// <summary>
+        /// Builds the RewardData represented by this builder's parameters recursively;
+        /// all Builders represented in this class's various fields will also be built.
+        /// </summary>
+        /// <returns>The newly created RewardData</returns>
+        public RewardData Build()
+        {
+            RewardData rewardData = ScriptableObject.CreateInstance<DraftRewardData>();
+            AccessTools.Field(typeof(GameData), "id").SetValue(rewardData, this.DraftRewardID);
+            AccessTools.Field(typeof(DraftRewardData), "classDataOverride").SetValue(rewardData, this.ClassDataOverride);
+            AccessTools.Field(typeof(DraftRewardData), "classType").SetValue(rewardData, this.ClassType);
+            AccessTools.Field(typeof(DraftRewardData), "classTypeOverride").SetValue(rewardData, this.ClassTypeOverride);
+            AccessTools.Field(typeof(DraftRewardData), "draftOptionsCount").SetValue(rewardData, this.DraftOptionsCount);
+            AccessTools.Field(typeof(DraftRewardData), "draftPool").SetValue(rewardData, this.DraftPool);
+            AccessTools.Field(typeof(DraftRewardData), "grantSingleCard").SetValue(rewardData, this.GrantSingleCard);
+            AccessTools.Field(typeof(DraftRewardData), "rarityFloorOverride").SetValue(rewardData, this.RarityFloorOverride);
+            AccessTools.Field(typeof(DraftRewardData), "useRunRarityFloors").SetValue(rewardData, this.UseRunRarityFloors);
+            AccessTools.Field(typeof(GrantableRewardData), "CanBeSkippedOverride").SetValue(rewardData, this.CanBeSkippedOverride);
+            AccessTools.Field(typeof(GrantableRewardData), "ForceContentUnlocked").SetValue(rewardData, this.ForceContentUnlocked);
+            AccessTools.Field(typeof(GrantableRewardData), "saveManager").SetValue(rewardData, this.SaveManager);
+            AccessTools.Field(typeof(RewardData), "costs").SetValue(rewardData, this.Costs);
+            AccessTools.Field(typeof(RewardData), "ShowRewardAnimationInEvent").SetValue(rewardData, this.ShowRewardAnimationInEvent);
+            AccessTools.Field(typeof(RewardData), "_collectSFXCueName").SetValue(rewardData, this._CollectSFXCueName);
+            AccessTools.Field(typeof(RewardData), "_isServiceMerchantReward").SetValue(rewardData, this._IsServiceMerchantReward);
+            if (this.Description != null)
+            {
+                this._RewardDescriptionKey = "DraftRewardData_" + this.DraftRewardID + "_RewardDescriptionKey";
+                // Use Description field for all languages
+                // This should be changed in the future to add proper localization support to custom content
+                CustomLocalizationManager.ImportSingleLocalization(this._RewardDescriptionKey, "Text", "", "", "", "", this.Description, this.Description, this.Description, this.Description, this.Description, this.Description);
+            }
+            AccessTools.Field(typeof(RewardData), "_rewardDescriptionKey").SetValue(rewardData, this._RewardDescriptionKey);
+            AccessTools.Field(typeof(RewardData), "_rewardSprite").SetValue(rewardData, this._RewardSprite);
+            if (this.Name != null)
+            {
+                this._RewardTitleKey = "DraftRewardData_" + this.DraftRewardID + "_RewardTitleKey";
+                // Use Name field for all languages
+                // This should be changed in the future to add proper localization support to custom content
+                CustomLocalizationManager.ImportSingleLocalization(this._RewardTitleKey, "Text", "", "", "", "", this.Name, this.Name, this.Name, this.Name, this.Name, this.Name);
+            }
+            AccessTools.Field(typeof(RewardData), "_rewardTitleKey").SetValue(rewardData, this._RewardTitleKey);
+            AccessTools.Field(typeof(RewardData), "_showCancelOverride").SetValue(rewardData, this._ShowCancelOverride);
+            AccessTools.Field(typeof(RewardData), "_showRewardFlowInEvent").SetValue(rewardData, this._ShowRewardFlowInEvent);
+            return rewardData;
+        }
+    }
+}
---
+++ MonsterTrainModdingAPI/Managers/CustomMapNodeManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonsterTrainModdingAPI.Builders;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+namespace MonsterTrainModdingAPI.Managers
+{
+    public class CustomMapNodeManager
+    {
+        /// <summary>
+        /// Maps map node pool IDs to the RewardNodeData of reward nodes which can appear in them.
+        /// Reward nodes which naturally appear in the pool in the base game will not appear in these lists.
+        /// </summary>
+        public static IDictionary<string, List<RewardNodeData>> CustomRewardNodeData { get; } = new Dictionary<string, List<RewardNodeData>>();
+        /// <summary>
+        /// Static reference to the game's SaveManager, which is necessary to register new map node data.
+        /// </summary>
+        public static SaveManager SaveManager { get; set; }
+
+        /// <summary>
+        /// Add the card to the card pools with given IDs.
+        /// </summary>
+        /// <param name="rewardNodeData">RewardNodeData to be added to the pools</param>
+        /// <param name="mapNodePoolIDs">List of map node pool IDs to add the reward node to</param>
+        public static void RegisterCustomRewardNode(RewardNodeData rewardNodeData, List<string> mapNodePoolIDs)
+        {
+            var mapNodeDatas = (List<MapNodeData>)AccessTools.Field(typeof(AllGameData), "mapNodeDatas").GetValue(SaveManager.GetAllGameData());
+            mapNodeDatas.Add(rewardNodeData);
+            foreach (string mapNodePoolID in mapNodePoolIDs)
+            {
+                if (!CustomRewardNodeData.ContainsKey(mapNodePoolID))
+                {
+                    CustomRewardNodeData[mapNodePoolID] = new List<RewardNodeData>();
+                }
+                CustomRewardNodeData[mapNodePoolID].Add(rewardNodeData);
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all reward nodes added to the given map node pool by mods,
+        /// then adds them to the list passed in.
+        /// Reward nodes which naturally appear in the pool will not be returned.
+        /// </summary>
+        /// <param name="mapNodePoolID">ID of the map node pool to get nodes for</param>
+        /// <param name="mapNodes">List of map nodes to add the nodes to</param>
+        /// <returns>A list of reward nodes added to the map node pool with given ID by mods</returns>
+        public static void AddRewardNodesForPool(string mapNodePoolID, List<MapNodeData> mapNodes)
+        {
+            if (CustomRewardNodeData.ContainsKey(mapNodePoolID))
+            {
+                mapNodes.AddRange(CustomRewardNodeData[mapNodePoolID]);
+            }
+        }
+    }
+}
