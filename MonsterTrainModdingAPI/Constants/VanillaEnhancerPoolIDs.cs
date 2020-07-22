3a7d68f6cc0329c984e7037d6d3e485ced72d002
Colin Noga <Tempus@chronometry.ca>
2020-07-21 18:50:46 -0700

    Enhancer data builder and Enhancer Pool Support (#63)
    * Enhancer Data Builder, using Traverse.
    
    * Pool Support for Enhancers. Tested and Complete
    
    * Update AddCustomEnhancerToPoolPatch.cs
    
    * Improving the rarity comment

---
+++ MonsterTrainModdingAPI/Constants/VanillaEnhancerPoolIDs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace MonsterTrainModdingAPI.Constants
+{
+    /// <summary>
+    /// Provides easy access to all of the base game's enhancer pool IDs
+    /// </summary>
+    public static class VanillaEnhancerPoolIDs
+    {
+        /// <summary>
+        /// The first slot in the Spell Upgrade Shop, always contains a -1 Ember upgrade.
+        /// </summary>
+        public static readonly string SpellUpgradePoolCostReduction = "SpellUpgradePoolCostReduction";
+        /// <summary>
+        /// The second slot in the Spell Upgrade Shop. Contains Magic Power +10 and Consume, Magic Power +20. 
+        /// </summary>
+        public static readonly string SpellUpgradePoolCommon = "SpellUpgradePoolCommon";
+        /// <summary>
+        /// The third slot in the Spell Upgrade Shop. Contains Holdover, remove Consume, Permafrost and Doublestack. Permafrost is Stygian themed but not limited.
+        /// </summary>
+        public static readonly string SpellUpgradePool = "SpellUpgradePool";
+        /// <summary>
+        /// The first two slots in the Unit Upgrade Shop. Contains +10 attack, +25 Health, and +10/+10. Also contains a class specific upgrade for your main and subclass.
+        /// </summary>
+        public static readonly string UnitUpgradePoolCommon = "UnitUpgradePoolCommon";
+        /// <summary>
+        /// The last slots in the Unit Upgrade Shop. Contains themed rare upgrades available for all classes. Multistrike (Hellhorned), Quick (Awoken), Largestone (Umbra) and Endless (Remnant). Stygian is in rare Spell upgrades.
+        /// </summary>
+        public static readonly string UnitUpgradePool = "UnitUpgradePool";
+    }
+}
