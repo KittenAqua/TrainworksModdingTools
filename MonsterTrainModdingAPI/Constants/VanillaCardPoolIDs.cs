e6178602c20a0c423ba93bda68c7670f52cdf073
KittenAqua <mnpntgt@gmail.com>
2020-10-19 09:47:54 -0400

    Add several card pools to the card pool constants file

--- MonsterTrainModdingAPI/Constants/VanillaCardPoolIDs.cs
+++ MonsterTrainModdingAPI/Constants/VanillaCardPoolIDs.cs
@@ -18,7 +18,43 @@ namespace MonsterTrainModdingAPI.Constants
         /// Use unknown
         /// </summary>
         public static readonly string UnitsAllBanner = "UnitsAllBanner";
-        
+        /// <summary>
+        /// Hellhorned banner pool
+        /// </summary>
+        public static readonly string HellhornedBanner = "UnitsHellhornedBanner";
+        /// <summary>
+        /// Awoken banner pool
+        /// </summary>
+        public static readonly string AwokenBanner = "UnitsAwokenBanner";
+        /// <summary>
+        /// Stygian banner pool
+        /// </summary>
+        public static readonly string StygianBanner = "UnitsStygianBanner";
+        /// <summary>
+        /// Umbra banner pool
+        /// </summary>
+        public static readonly string UmbraBanner = "UnitsUmbraBanner";
+        /// <summary>
+        /// Melting Remnant banner pool
+        /// </summary>
+        public static readonly string RemnantBanner = "UnitsRemnantBanner";
+        /// <summary>
+        /// Pool containing all champions
+        /// </summary>
+        public static readonly string ChampionPool = "ChampionPool";
+        /// <summary>
+        /// Pool used when an effect randomly generates an imp
+        /// </summary>
+        public static readonly string ImpPool = "ImpPool";
+        /// <summary>
+        /// Pool used when an effect randomly generates a morsel
+        /// </summary>
+        public static readonly string MorselPool = "Class5Food";
+        /// <summary>
+        /// Pool used when one of the Umbra starter cards randomly generates a morsel
+        /// </summary>
+        public static readonly string MorselPoolStarter = "Class5StarterFoodCard";
+
         // Note: list incomplete. There are many more cardpools than just these two.
     }
 }
