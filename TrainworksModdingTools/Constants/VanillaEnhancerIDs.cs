bc7393ed49a767ff8ca2c5501f5182d525d5c6a2
KittenAqua <35976163+KittenAqua@users.noreply.github.com>
2020-10-21 06:59:46 -0400

    Add utilities to help add effects to enhancers (#116)

---
+++ TrainworksModdingTools/Constants/VanillaEnhancerIDs.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainworksModdingTools.Constants
+{
+    class VanillaEnhancerIDs
+    {
+        /// <summary>
+        /// Asset name: GenericDuplicator
+        /// </summary>
+        public static readonly string CardDuplicator = "81be1dda-5523-4e26-af12-03ae8c9ce262";
+        /// <summary>
+        /// Asset name: SpellDuplicator
+        /// </summary>
+        public static readonly string DuplicateSpell = "584ed916-e6b5-4483-b9b7-be25f730b45e";
+        /// <summary>
+        /// Asset name: SpellMagicPower
+        /// </summary>
+        public static readonly string Powerstone = "015f4d9d-3a87-4053-8e30-45a80fdf78ee";
+        /// <summary>
+        /// Asset name: SpellMagicPowerBigExtraCost
+        /// </summary>
+        public static readonly string Surgestone = "07de18ca-a585-4200-b139-63c5d4661140";
+        /// <summary>
+        /// Asset name: SpellUpgradeReduceCost
+        /// </summary>
+        public static readonly string EmberstoneSpell = "7301ebb3-4d99-46b9-b4bd-aeabf530fd0a";
+        /// <summary>
+        /// Asset name: SpellUpgradeReduceCostCopyDEPRECATED
+        /// </summary>
+        public static readonly string EmberstoneDEPRECATED = "3a1021b3-807e-462d-a3aa-45d3a5356b97";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitAddIntrinsicDEPRECATED
+        /// </summary>
+        public static readonly string Seekstone = "51277981-c504-434e-9698-1277dd5bc1f4";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitAddJuice
+        /// </summary>
+        public static readonly string Stackstone = "72f61ae8-7e0f-4066-a3fb-a1273f3aa273";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitAddPermafrost
+        /// </summary>
+        public static readonly string Freezestone = "a13a7545-0bc8-4c30-aa0e-da651894cebb";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitAddPiercingDEPRECATED
+        /// </summary>
+        public static readonly string Truestone = "fb9ac600-7c4c-4a48-8951-91d354394169";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitAddRetain
+        /// </summary>
+        public static readonly string Keepstone = "0454db73-87f1-4767-ae96-053928acc008";
+        /// <summary>
+        /// Asset name: SpellUpgradeTraitRemoveConsume
+        /// </summary>
+        public static readonly string EternalstoneRemoveConsume = "4cd37328-eb6e-4c4e-aa61-53492dfe8035";
+        /// <summary>
+        /// Asset name: UnitDuplicator
+        /// </summary>
+        public static readonly string UnitReplicator = "1f97beaf-81ab-40ad-819f-108ea86780bd";
+        /// <summary>
+        /// Asset name: UnitUpgradeAddEndlessRemnant
+        /// </summary>
+        public static readonly string EternalstoneAddEndless = "2ce018b5-2b43-4ecf-8567-696d4fd8c119";
+        /// <summary>
+        /// Asset name: UnitUpgradeBiggerUmbra
+        /// </summary>
+        public static readonly string Largestone = "e1646fd5-9245-431a-a07f-580c3a033347";
+        /// <summary>
+        /// Asset name: UnitUpgradeClanAwokenAddSpikes
+        /// </summary>
+        public static readonly string Thornstone = "16beb502-1f53-42c0-b4fb-61da62401475";
+        /// <summary>
+        /// Asset name: UnitUpgradeClanHellhornedAddRage
+        /// </summary>
+        public static readonly string Furystone = "d00f4776-f11a-4c9c-a62f-bf9a95eaa52a";
+        /// <summary>
+        /// Asset name: UnitUpgradeClanRemnantAddBurnout
+        /// </summary>
+        public static readonly string Wickstone = "ba976851-189e-4499-8e22-3fd76914c382";
+        /// <summary>
+        /// Asset name: UnitUpgradeClanStygianIncantArmor
+        /// </summary>
+        public static readonly string Runestone = "70dbb13a-a4f0-44bd-bcd5-9c5b94860cf5";
+        /// <summary>
+        /// Asset name: UnitUpgradeClanUmbraDamageShield
+        /// </summary>
+        public static readonly string Shieldstone = "3ec7e45c-0820-45b7-910c-e50e817bcf23";
+        /// <summary>
+        /// Asset name: UnitUpgradeInherentTraitAdd
+        /// </summary>
+        public static readonly string Summonstone = "90a9bd0f-a35a-49e1-80c6-7228505c725b";
+        /// <summary>
+        /// Asset name: UnitUpgradeLowerCost
+        /// </summary>
+        public static readonly string EmberstoneUnit = "d27a11b4-84b5-4082-b75a-d20badf0ca31";
+        /// <summary>
+        /// Asset name: UnitUpgradeMultistrikeHellhorned
+        /// </summary>
+        public static readonly string Frenzystone = "0016b165-11a9-4a26-8837-3b2895bc39f8";
+        /// <summary>
+        /// Asset name: UnitUpgradeQuickAwoken
+        /// </summary>
+        public static readonly string Speedstone = "74d5a105-cc4a-4de4-8189-d346e2a6de53";
+        /// <summary>
+        /// Asset name: UnitUpgradeSmaller
+        /// </summary>
+        public static readonly string Smallstone = "0690d498-a4cd-46fc-b9ae-18fcc34a4b6b";
+        /// <summary>
+        /// Asset name: UnitUpgradeStatsAttack
+        /// </summary>
+        public static readonly string Strengthstone = "e8095d27-518c-4dd8-9f19-4306b42940f9";
+        /// <summary>
+        /// Asset name: UnitUpgradeStatsBoth
+        /// </summary>
+        public static readonly string Battlestone = "247c99a2-1076-4cef-be8a-1b8441ea75d3";
+        /// <summary>
+        /// Asset name: UnitUpgradeStatsHealth
+        /// </summary>
+        public static readonly string Heartstone = "f0c610cd-0b6b-48af-8c61-9d23dbdfea34";
+        /// <summary>
+        /// Asset name: UnitUpgradeTrampleUmbra
+        /// </summary>
+        public static readonly string TrampleUmbra = "d11e1174-521a-4aa1-bc78-538645c2ce94";
+        /// <summary>
+        /// Asset name: CardDuplicator
+        /// </summary>
+        public static readonly string CardReplicator = "88a1c4c8-b3e6-4e1e-8b3f-f65fe5c09882";
+        /// <summary>
+        /// Asset name: CardDuplicator0CostAnnihilate
+        /// </summary>
+        public static readonly string CardReplicator0CostAnnihilate = "15b51631-7f09-42a7-8041-341f676fa491";
+        /// <summary>
+        /// Asset name: UpgradeLarge_ShrinkRayEvent
+        /// </summary>
+        public static readonly string MajorRefraction = "bb9f4b80-45be-4aad-811b-5c5450a5c11f";
+        /// <summary>
+        /// Asset name: UpgradeMagicPowerExhaust
+        /// </summary>
+        public static readonly string SpellRailspike = "fc38cd31-4aa5-4710-81ac-12b3c6611389";
+        /// <summary>
+        /// Asset name: UpgradeMajorBuffExhaust
+        /// </summary>
+        public static readonly string MonsterRailspike = "af3618de-70d1-4b2e-980b-4b85fbcf83f4";
+        /// <summary>
+        /// Asset name: UpgradeReplicatorsAnnhilate
+        /// </summary>
+        public static readonly string ImprovedSpikedriver = "481b0afa-6b62-4abf-9b92-a3e68c501c08";
+        /// <summary>
+        /// Asset name: UpgradeReplicatorSpellAnnhilate
+        /// </summary>
+        public static readonly string ImprovedRailspike = "eeef8380-bc30-49a5-aff0-a125599bb554";
+        /// <summary>
+        /// Asset name: UpgradeSmall_ShrinkRayEvent
+        /// </summary>
+        public static readonly string MinorRefraction = "3e025f03-d52f-494a-a61e-c8ec504d1061";
+    }
+}
