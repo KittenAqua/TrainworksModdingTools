9c5b7998fbc94f27d90b4d98151d8c3d0dc4808f
Steven Lilley <steven.b.lilley@gmail.com>
2021-08-04 15:40:25 -0400

    Created foundation of patch whose main function is to call the CollectMappingData method from within the UnitSynthesisMapping class to reset the editorMappings list. If performed after adding custom CharacterData and custom CardUpgradeData appropriatly, can be used to create unit synthesis connections within the TLD expansion.

---
+++ TrainworksModdingTools/Patches/ResetUnitSynthesisMapping.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using Trainworks.Managers;
+using System;
+
+namespace Trainworks.Patches
+{
+    class AccessUnitSynthesisMapping
+    {
+        public static void FindUnitSynthesisMappingInstanceToStub()
+        {
+            // Gets a reference to AllGameData with Trainworks
+            AllGameData testData = ProviderManager.SaveManager.GetAllGameData();
+
+            // Use AllGameData to get access to BalanceData
+            BalanceData balanceData = testData.GetBalanceData();
+
+            // Use BalanceData to get access to the current instance of the UnitSynthesisMapping
+            UnitSynthesisMapping mappingInstance = balanceData.SynthesisMapping;
+            if (mappingInstance == null)
+            {
+                Trainworks.Log("Failed to find a mapping instance.");
+            }
+            else
+            {
+                Trainworks.Log("Able to find mapping instance: " + mappingInstance.GetID()); // Test to see if this is a different instance
+            }
+
+            // Calls CollectMappingData method
+            RecallingCollectMappingData.CollectMappingDataStub(mappingInstance);
+            Trainworks.Log("Called CollectMappingData.");
+        }
+    }
+
+
+    // Solely exists to allow calling of private method CollectMappingData from within a UnitSynthesisMapping instance
+    // This resets the editorMappings list within the UnitSynthesisMapping class in Monster Train
+    // Effective for creating unit synthesis connections in TLD DLC after appropriate custom CharacterData and custom CardUpgradeData has been added to AllGameData
+    // *Required: Called after adding custom content to work for this purpose
+    [HarmonyPatch(typeof(UnitSynthesisMapping), "CollectMappingData", new Type[] { })]
+    public class RecallingCollectMappingData
+    {
+        [HarmonyReversePatch]
+        public static void CollectMappingDataStub(object instance)
+        {
+            // It's a stub so it has no initial content
+        }
+    }
+}
