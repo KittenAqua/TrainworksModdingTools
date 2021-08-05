a2243faa315a34d27d435b3ea61c49a27cfd59fd
Steven Lilley <steven.b.lilley@gmail.com>
2021-08-05 11:02:18 -0400

    Reorganized comments and notes.

--- TrainworksModdingTools/Patches/ResetUnitSynthesisMapping.cs
+++ TrainworksModdingTools/Patches/ResetUnitSynthesisMapping.cs
@@ -18,7 +18,7 @@ namespace Trainworks.Patches
             UnitSynthesisMapping mappingInstance = balanceData.SynthesisMapping;
             if (mappingInstance == null)
             {
-                Trainworks.Log("Failed to find a mapping instance.");
+                Trainworks.Log(BepInEx.Logging.LogLevel.Warning, "Failed to find a mapping instance.");
             }
             else
             {
@@ -27,15 +27,17 @@ namespace Trainworks.Patches
 
             // Calls CollectMappingData method
             RecallingCollectMappingData.CollectMappingDataStub(mappingInstance);
-            Trainworks.Log("Called CollectMappingData.");
         }
     }
 
-
-    // Solely exists to allow calling of private method CollectMappingData from within a UnitSynthesisMapping instance
-    // This resets the editorMappings list within the UnitSynthesisMapping class in Monster Train
-    // Effective for creating unit synthesis connections in TLD DLC after appropriate custom CharacterData and custom CardUpgradeData has been added to AllGameData
-    // *Required: Called after adding custom content to work for this purpose
+    /// <summary>
+    /// Solely exists to allow calling of private method CollectMappingData from within a UnitSynthesisMapping instance
+    /// This resets the editorMappings list within the UnitSynthesisMapping class in Monster Train
+    /// Effective for creating unit synthesis connections in TLD DLC after appropriate custom CharacterData and custom CardUpgradeData has been added to AllGameData
+    /// *Required: Called after adding custom content to work for this purpose
+    /// Current Use: Each mod will have access to this Trainworks method so they can call it after adding all their content (At the end of their Initialize method generally works)
+    /// Ideally: This is called a single time after all mods/plugins have added their custom content
+    /// </summary>
     [HarmonyPatch(typeof(UnitSynthesisMapping), "CollectMappingData", new Type[] { })]
     class RecallingCollectMappingData
     {
