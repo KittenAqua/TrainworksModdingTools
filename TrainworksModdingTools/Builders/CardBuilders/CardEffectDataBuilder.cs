b112dde2561f605a2a5908125d270c58597fce71
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2021-08-09 21:07:21 -0400

    Custom Unit Synthesis Patcher (#142)
    * Created foundation of patch whose main function is to call the CollectMappingData method from within the UnitSynthesisMapping class to reset the editorMappings list. If performed after adding custom CharacterData and custom CardUpgradeData appropriatly, can be used to create unit synthesis connections within the TLD expansion.
    
    * Made the class accessible so it's easy to call with any mod using Trainworks.
    
    * Reorganized comments and notes.
    
    * This is a gross way to do this, but I wanted to point out that the approach of using the CollectMappingData method from within UnitSynthesisMapping specifically checks for this exact string name in a card's list of card effects. So if this is not explicitily set by the user AFTER setting the EffectStateType (since this implicitly sets the EffectStateName), that approach will not work as this effect will have the extremely long and detailed AssemblyQualifiedName.
    
    * Another attempt at a modder's QOL fix when using the CollectMappingData UnitSynthesisMapping approach. It appeared that Trainworks will remove any CardUpgradeData with a duplicate UpgradeTitleKey to prevent repetitive upgrades, which would lead to removing many CardUpgradeData objects (all except the final one) when the UpgradeTitleKey was not set (since they would all have the same exact string name here). The way this was originally setup guarantees that if the null case is already met, all the strings are set to be the same since it is just adding NULL to the additional string. This way will at least set unique values for all of these as long as the modder sets the UpgradeTitle.
    
    * Quick test to see if there was an easy way to specifically have CardEffectSpawnMonster card effect specifically set its EffectStateName to "CardEffectSpawnMonster" and this appeared to do the trick. Again, I have not done significant testing or research to see how critical AssemblyQualifiedName is here for other aspects of the game, but I just wanted to point out a potential point of entry to help set default values specifically with the CollectMappingData method in mind to keep modders from running into issues.

--- TrainworksModdingTools/Builders/CardBuilders/CardEffectDataBuilder.cs
+++ TrainworksModdingTools/Builders/CardBuilders/CardEffectDataBuilder.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using ShinyShoe;
 using Trainworks.Managers;
+using System.Runtime.CompilerServices;
 
 namespace Trainworks.Builders
 {
@@ -29,7 +30,10 @@ namespace Trainworks.Builders
             set
             {
                 this.effectStateType = value;
-                this.EffectStateName = this.effectStateType.AssemblyQualifiedName;
+                // I was unclear if the AssemblyName was critical here, so just wanted to point out a potential spot to specifically make sure CardEffectMonsterSpawn gave exactly the proper string even if modder did not specify.
+                // Quick testing showed no ill effects, but could understand if there were more working behind the scenes I missed.
+                // Modders can also just specifically set the EffectStateName AFTER setting the EffectStateType in their CardEffectDataBuilder, but wanted to find a way to protect them from themselves
+                this.EffectStateName = this.effectStateType.Name;
             }
         }
 
---
+++ TrainworksModdingTools/Patches/ResetUnitSynthesisMapping.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using Trainworks.Managers;
+using System;
+
+namespace Trainworks.Patches
+{
+    public class AccessUnitSynthesisMapping
+    {
+        public static void FindUnitSynthesisMappingInstanceToStub()
+        {
+            // Gets a reference to AllGameData with Trainworks
+            AllGameData testData = ProviderManager.SaveManager.GetAllGameData();
+
+            // Use AllGameData to get access to BalanceData
+            BalanceData balanceData = testData.GetBalanceData();
+
+            // Use BalanceData to get access to the current instance of the UnitSynthesisMapping
+            UnitSynthesisMapping mappingInstance = balanceData.SynthesisMapping;
+            if (mappingInstance == null)
+            {
+                Trainworks.Log(BepInEx.Logging.LogLevel.Warning, "Failed to find a mapping instance.");
+            }
+            else
+            {
+                Trainworks.Log("Able to find mapping instance: " + mappingInstance.GetID()); // Test to see if this is a different instance
+            }
+
+            // Calls CollectMappingData method
+            RecallingCollectMappingData.CollectMappingDataStub(mappingInstance);
+        }
+    }
+
+    /// <summary>
+    /// Solely exists to allow calling of private method CollectMappingData from within a UnitSynthesisMapping instance
+    /// This resets the editorMappings list within the UnitSynthesisMapping class in Monster Train
+    /// Effective for creating unit synthesis connections in TLD DLC after appropriate custom CharacterData and custom CardUpgradeData has been added to AllGameData
+    /// *Required: Called after adding custom content to work for this purpose
+    /// Current Use: Each mod will have access to this Trainworks method so they can call it after adding all their content (At the end of their Initialize method generally works)
+    /// Ideally: This is called a single time after all mods/plugins have added their custom content
+    /// </summary>
+    [HarmonyPatch(typeof(UnitSynthesisMapping), "CollectMappingData", new Type[] { })]
+    class RecallingCollectMappingData
+    {
+        [HarmonyReversePatch]
+        public static void CollectMappingDataStub(object instance)
+        {
+            // It's a stub so it has no initial content
+        }
+    }
+}
