9bd8b37067d21a2f1543aa94a6295bed5f8a1de0
Brandon <tricksterguy87@gmail.com>
2023-04-16 16:53:27 -0700

    Add Constants for Card Trigger Effect types.

---
+++ TrainworksModdingTools/Constants/VanillaCardTriggerEffectTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.Constants
+{
+    /// <summary>
+    /// Provides easy access to all of the base game's card trigger effect types
+    /// </summary>
+    public static class VanillaCardTriggerEffectTypes
+    {
+        public static readonly Type CardTriggerEffectBuffCharacterDamage = typeof(CardTriggerEffectBuffCharacterDamage);
+        public static readonly Type CardTriggerEffectBuffCharacterMaxHP = typeof(CardTriggerEffectBuffCharacterMaxHP);
+        public static readonly Type CardTriggerEffectBuffSpellDamage = typeof(CardTriggerEffectBuffSpellDamage);
+        public static readonly Type CardTriggerEffectDrawAdditionalNextTurn = typeof(CardTriggerEffectDrawAdditionalNextTurn);
+        public static readonly Type CardTriggerEffectGainEnergyNextTurn = typeof(CardTriggerEffectGainEnergyNextTurn);
+    }
+}
